140b965e3a759f1eeccecd90f90a71d4486cdac1
Alexander Lunegov <alunegov@gmail.com>
2019-03-23 21:25:45 +0500

    chore: отображение счетчика fps в верхнем углу экрана

--- app/src/main/java/com/github/alunegov/tchart/DebugApplication.java
+++ app/src/main/java/com/github/alunegov/tchart/DebugApplication.java
@@ -7,7 +7,11 @@ import com.codemonkeylabs.fpslibrary.TinyDancer;
 public class DebugApplication extends Application {
     @Override
     public void onCreate() {
+        super.onCreate();
+
         TinyDancer.create()
+                .startingXPosition(0)
+                .startingYPosition(0)
                 .show(this);
     }
 }
